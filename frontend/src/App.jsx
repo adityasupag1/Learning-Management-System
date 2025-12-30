7465906b716b4ea12035d7b5a871abbe3c4a3e0b
Aditya Raj <araj15397@gmail.com>
2025-12-30 22:15:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-9xl'>
-      App
-    </div>
+   <>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+
+     </Routes>
+   </>
   )
 }
 
