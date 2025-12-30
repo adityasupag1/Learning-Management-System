25764fd39a11125561d8e6a07260da33cfb97f79
Aditya Raj <araj15397@gmail.com>
2025-12-31 03:09:25 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import { Route, Routes } from 'react-router-dom'
+function App() {
+
+
+  return (
+    <>
+       <Routes>
+        <Route path="/" element = {<Home/>} />
+        <Route path="/signup" element = {<Signup/>} />
+        <Route path="/login" element = {<Login/>} />
+       </Routes>
+    </>
+  )
+}
+
+export default App
