57b3d9cb902468544950060ce5e78c4656937094
Aditya Raj <araj15397@gmail.com>
2025-12-31 05:48:00 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,7 +3,12 @@ import { IoMdEye } from "react-icons/io";
 import { IoMdEyeOff } from "react-icons/io";
 import logo from "../assets/logo.jpg";
 import google from "../assets/google.jpg";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
+import {ClipLoader} from 'react-spinners';
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -11,17 +16,45 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("student");
   const [show, setShow] = useState(true);
+  cosnt [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  const {serverUrl} = useContext(UserContext);
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        `${serverUrl}/api/auth/signup`,
+        { name, email, password, role },
+        { withCredentials: true }
+      );
+
+      console.log(res.data);
+      setLoading(false);
+      navigate("/login");
+      toast.success("Signup Successfully");
+      
+    } catch (error) {
+      setLoading(false);
+      if (error.response) {
+       toast.error(error.response.data.message); // "Email Already Exist"
+      } else {
+       toast.error("Server not responding");
+      }
+    }
+  };
 
   return (
     <div className="bg-[#dddbdb] w-screen h-screen flex justify-center items-center">
-      <form className="w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex ">
+      <form onSubmit={handleSignup} className="w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex ">
         {/* left div */}
-        <div className="md:w-[50%] w-full h-full flex flex-col items-center justify-center gap-3">
+        <div className="md:w-[50%] w-full h-full flex flex-col items-center justify-center text-center gap-3">
           {/* heading */}
           <div>
             <h1 className="font-semibold text-black text-2xl">Let's get started </h1>
             <h2 className="text-[#999797] text-4.5 ">Create your account</h2>
+         
           </div>
 
           {/* name input */}
@@ -31,6 +64,8 @@ const Signup = () => {
             </label>
             <input
               type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="border w-full h-8.75 border-[#e7e6e6] text-3.75 px-5"
               placeholder="Your Name"
             />
@@ -43,6 +78,8 @@ const Signup = () => {
             </label>
             <input
               type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="border w-full h-8.75 border-[#e7e6e6] text-3.75 px-5"
               placeholder="Enter Email"
             />
@@ -55,6 +92,8 @@ const Signup = () => {
             </label>
             <input
               type={show ? "password" : "text"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="border w-full h-8.75 border-[#e7e6e6] text-3.75 px-5"
               placeholder="Enter Password"
             />
@@ -67,19 +106,34 @@ const Signup = () => {
 
           {/* role input */}
           <div className="flex md:w-[50%] w-[70%] items-center justify-between">
-            <span className="px-2.5 py-1.25 border-2 border-[#e7e6e6] rounded-xl cursor-pointer hover:border-black">
+            <span
+              onClick={() => setRole("student")}
+              className={`px-2.5 py-1.25 border-2 border-[#e7e6e6] rounded-xl cursor-pointer  ${
+                role === "student" ? "border-black" : ""
+              }`}
+            >
               Student
             </span>
 
-            <span className="px-2.5 py-1.25 border-2 border-[#e7e6e6] rounded-xl cursor-pointer hover:border-black">
+            <span
+              onClick={() => setRole("educator")}
+              className={`px-2.5 py-1.25 border-2 border-[#e7e6e6] rounded-xl cursor-pointer  ${
+                role === "educator" ? "border-black" : ""
+              }`}
+            >
               Educator
             </span>
           </div>
 
           {/* signup button */}
-          <button className="w-[80%] h-10 bg-black text-white cursor-pointer flex justify-center items-center rounded-[5px]">
-            Signup
+          <button
+            type="submit"
+             disabled={loading}
+            className="w-[80%] h-10 bg-black text-white cursor-pointer flex justify-center items-center rounded-[5px]"
+          >
+           {loading ? <ClipLoader size={25} color={"#ffffff"} /> : "Sign Up"}
           </button>
+
 
           {/* or continue with */}
           <div className="w-[80%] flex items-center gap-2">
