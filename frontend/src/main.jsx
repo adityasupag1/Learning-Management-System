319cf6425c28b491036b85e86d0569f202d2f223
Aditya Raj <araj15397@gmail.com>
2026-01-09 06:55:09 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import UserProvider from "./context/userContext";
+import store from './redux/store.js'
+import {Provider} from 'react-redux'
 
 createRoot(document.getElementById("root")).render(
-  <UserProvider>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </UserProvider>
+    </Provider>
+  </BrowserRouter>
 );
