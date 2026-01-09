cbdb99033b4607c658f925c196377b5cfb595350
Aditya Raj <araj15397@gmail.com>
2026-01-09 06:55:21 +0530

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,1 +1,9 @@
- 
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import userSlice from '../redux/slices/userSlice'
+const store = configureStore({
+       reducer : {
+         user : userSlice
+       }
+})
+
+export default store
