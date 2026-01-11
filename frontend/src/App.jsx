118ec3dbb5251437c907251a1fe89e8e1ee28a62
Aditya Raj <araj15397@gmail.com>
2026-01-12 05:16:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
-import Home from './pages/Home'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import { Route, Routes } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
-import getCurrentUser from './customHooks/getCurrentUser'
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import useGetCurrentUser from "./customHooks/useGetCurrentUser";
+
 function App() {
-  
-  getCurrentUser();
+  useGetCurrentUser(); // ✅ correct usage
+
   return (
     <>
-     <ToastContainer/>
-       <Routes>
-        <Route path="/" element = {<Home/>} />
-        <Route path="/signup" element = {<Signup/>} />
-        <Route path="/login" element = {<Login/>} />
-       </Routes>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
