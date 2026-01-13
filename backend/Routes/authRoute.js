ed4c1911f37bd70df46085a3d00164ddc55c864d
Aditya Raj <araj15397@gmail.com>
2026-01-13 05:42:35 +0530

    Update authRoute.js

--- backend/Routes/authRoute.js
+++ backend/Routes/authRoute.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
 const authRouter = express.Router();
-const {signupController, loginController, logoutController}= require('../controllers/authController')
+const {signupController, loginController, logoutController,sendOtpController, verifyOtpController,resetPasswrodController}= require('../controllers/authController')
 authRouter.post('/signup', signupController)
 authRouter.post('/login', loginController)
 authRouter.get('/logout', logoutController)
+authRouter.post('/sendotp',sendOtpController);
+authRouter.post('/verifyotp',verifyOtpController);
+authRouter.post('/resetpassword',resetPasswrodController);
 
 module.exports=authRouter
