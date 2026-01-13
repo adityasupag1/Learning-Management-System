2493c950577ee1c0695283b05974fd313861a00a
Aditya Raj <araj15397@gmail.com>
2026-01-13 05:42:40 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import useGetCurrentUser from "./customHooks/useGetCurrentUser";
+import { useSelector } from "react-redux";
+import Profile from "./pages/Profile";
+import ForgetPassword from "./pages/ForgetPassword";
 
 function App() {
   useGetCurrentUser(); // ✅ correct usage
-
+  const { userData } = useSelector((state) => state.user);
   return (
     <>
       <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={!userData ? <Signup /> : <Navigate to={"/"} />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={userData ? <Profile /> : <Navigate to={"/signup"} />} />
+        <Route path="/forgotpassword" element={userData ? <ForgetPassword /> : <Navigate to={"/signup"} />} />
       </Routes>
     </>
   );
